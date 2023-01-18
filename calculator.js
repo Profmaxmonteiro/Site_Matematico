3446fb3f29fc5bdc37bb71e302b5498a7db28ba2
Profmaxmonteiro <88830443+Profmaxmonteiro@users.noreply.github.com>
2023-01-18 13:32:18 -0300

    Delete calculator.js
    Signed-off-by: Profmaxmonteiro <88830443+Profmaxmonteiro@users.noreply.github.com>

--- calculator.js
+++
@@ -1,5 +0,0 @@
-function calculate() {
-  var expression = document.getElementById("expression").value;
-  var result = math.evaluate(expression);
-  document.getElementById("result").innerHTML = expression + " = " + result;
-}
